d3c9b053921667d7131b1f353fd9bf1afe80506a
Dave9123 <dave.turmawan@outlook.com>
2024-01-03 12:53:26 +0700

    :)

--- server.js
+++ server.js
@@ -2,24 +2,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Serve static files (your HTML, CSS, and images)
 app.use(express.static(path.join(__dirname, 'public_html')));
-
-// API endpoint to get search results from Crazy Games
-app.get('/api/search', async (req, res) => {
+app.get('/search', async (req, res) => {
+ if (req.query.q) {
   try {
     const searchTerm = req.query.q;
-    const limit = req.query.limit || 10; // Set a default limit if not provided
-
-    // Make a request to the Crazy Games API using fetch
+    const limit = req.query.limit || 10;
     const apiUrl = `https://api.crazygames.com/v3/en_US/search?q=${searchTerm}&limit=${limit}&device=desktop&includeTopGames=true`;
     const response = await fetch(apiUrl);
 
     if (!response.ok) {
       throw new Error(`Failed to fetch data from CrazyGames API (${response.status} ${response.statusText})`);
     }
-
-    // Parse JSON from the response
     const data = await response.json();
 
     // Extract relevant data from the response
@@ -34,16 +28,17 @@ app.get('/api/search', async (req, res) => {
       iosFriendly: result.iosFriendly,
       recordType: result.recordType,
     }));
-
     res.json(searchResults);
   } catch (error) {
     console.error('Error fetching search results:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
+ } else {
+  res.redirect('/')
+ }
 });
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running http://localhost:${PORT}`);
+ console.log(`Server is running http://localhost:${PORT}`);
 });
