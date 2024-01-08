d2df2b359734b2dc254ee8a74580722f84a3a0be
Dave9123 <dave.turmawan@outlook.com>
2024-01-08 17:36:37 +0700

    API finished probably, going to figure out how to show the game

--- server.js
+++ server.js
@@ -3,42 +3,40 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public_html')));
+
 app.get('/search', async (req, res) => {
- if (req.query.q) {
-  try {
-    const searchTerm = req.query.q;
-    const limit = req.query.limit || 10;
-    const apiUrl = `https://api.crazygames.com/v3/en_US/search?q=${searchTerm}&limit=${limit}&device=desktop&includeTopGames=true`;
-    const response = await fetch(apiUrl);
-
-    if (!response.ok) {
-      throw new Error(`Failed to fetch data from CrazyGames API (${response.status} ${response.statusText})`);
+  if (req.query.q) {
+    try {
+      const searchTerm = req.query.q;
+      const limit = req.query.limit || 10;
+      const apiUrl = `https://api.crazygames.com/v3/en_US/search?q=${searchTerm}&limit=${limit}&includeTopGames=true`;
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch data from CrazyGames API (${response.status} ${response.statusText})`);
+      }
+      const data = await response.json();
+      const searchResults = data.result
+        .filter(result => result.recordType !== 'tag')
+        .map(result => ({
+          title: result.name,
+          gamelink: `https://www.crazygames.com/game/${result.slug}`,
+          directGame: `https://games.crazygames.com/en-US/${result.slug}/index.html`,
+          cover: result.cover,
+          mobileFriendly: result.mobileFriendly,
+          androidFriendly: result.androidFriendly,
+          iosFriendly: result.iosFriendly,
+        }));
+      res.json(searchResults);
+    } catch (error) {
+      console.error('Error fetching search results:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
-    const data = await response.json();
-
-    // Extract relevant data from the response
-    const searchResults = data.result.map(result => ({
-      title: result.name,
-      slug: result.slug,
-      cover: result.cover,
-      videos: result.videos,
-      https: result.https,
-      mobileFriendly: result.mobileFriendly,
-      androidFriendly: result.androidFriendly,
-      iosFriendly: result.iosFriendly,
-      recordType: result.recordType,
-    }));
-    res.json(searchResults);
-  } catch (error) {
-    console.error('Error fetching search results:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+  } else {
+    res.redirect('/');
   }
- } else {
-  res.redirect('/')
- }
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
- console.log(`Server is running http://localhost:${PORT}`);
+  console.log(`Server is running http://localhost:${PORT}`);
 });
