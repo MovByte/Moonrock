58e543bd96a3e18ff142720a69d4e9ec9cb80740
Dave9123 <dave.turmawan@outlook.com>
2024-01-22 19:17:24 +0700

    Stuff I guess?

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
 const querylimit = process.env.QUERY_LIMIT || 10;
 
 app.use(express.static(path.join(__dirname, 'public_html')));
