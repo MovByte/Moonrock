975f66cdab4b25548241b9b1e728f674f63900ca
Dave9123 <dave.turmawan@outlook.com>
2024-02-05 21:02:46 +0700

    aaa scraping yandex

--- server.js
+++ server.js
@@ -20,17 +20,18 @@ app.use('/flash', async (req, res) => {
 //app.use('/yandex', async (req, res) => {
 //  const id = req.query.appID;
 //  const get = `https://yandex.com/games/app/${id}`;
-//  const response = await fetch("https://yandex.com/games/app/193229", {
+//  const response = await fetch(get, {
 //    "credentials": "include",
 //    "headers": {
-//        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0",
-//        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-//        "Accept-Language": "en,en-US;q=0.5",
-//        "Prefer": "safe",
-//        "Upgrade-Insecure-Requests": "1",
-//        "Sec-Fetch-Dest": "document",
-//        "Sec-Fetch-Mode": "navigate",
-//        "Sec-Fetch-Site": "cross-site"
+//      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0",
+//      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+//      "Accept-Language": "en,en-US;q=0.5",
+//      "Prefer": "safe",
+//      "Upgrade-Insecure-Requests": "1",
+//      "Sec-Fetch-Dest": "document",
+//      "Sec-Fetch-Mode": "navigate",
+//      "Sec-Fetch-Site": "none",
+//      "Sec-Fetch-User": "?1"
 //    },
 //    "method": "GET",
 //    "mode": "cors"
@@ -41,7 +42,9 @@ app.use('/flash', async (req, res) => {
 //  } else {
 //    const html = await response.text();
 //    // https://app-193229.games.s3.yandex.net/193229/xl4j3o6bypgxe6bekum7rl198bopqwhj/index.html
-//    await fs.writeFile(`debug/yandex-${id}.html`, html);
+//    await fs.ensureDir('debug');
+//    fs.writeFile(`debug/yandex-${id}.html`, html);
+//    res.send(html);
 //  }
 //});
 app.use('/search', async (req, res) => {
