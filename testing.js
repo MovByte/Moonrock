9dd453ff277c0e97b2ecd3c43951a8eb91395079
Dave9123 <dave.turmawan@outlook.com>
2024-02-08 22:33:50 +0700

    Finished making Discord login, so happy

--- testing.js
+++ testing.js
@@ -24,11 +24,11 @@ app.get('/auth/discord', passport.authenticate('discord'));
 app.get('/auth/discord/callback', async (req, res) => {
     const code = req.query.code;
     const params = new URLSearchParams();
-    params.append('client_id', process.env.DISCORD_CLIENT_ID); // Use environment variable
-    params.append('client_secret', process.env.DISCORD_CLIENT_SECRET); // Use environment variable
+    params.append('client_id', process.env.DISCORD_CLIENT_ID);
+    params.append('client_secret', process.env.DISCORD_CLIENT_SECRET);
     params.append('grant_type', 'authorization_code');
     params.append('code', code);
-    params.append('redirect_uri', process.env.DISCORD_CALLBACK_URL); // Use environment variable
+    params.append('redirect_uri', process.env.DISCORD_CALLBACK_URL);
 
     try {
         const response = await axios.post('https://discord.com/api/oauth2/token', params);
@@ -39,26 +39,13 @@ app.get('/auth/discord/callback', async (req, res) => {
                 authorization: `${token_type} ${access_token}`
             }
         });
-
-        const user = {
-            username: userDataResponse.data.username,
-            email: userDataResponse.data.email,
-            avatar: userDataResponse.data.avatar ? `https://cdn.discordapp.com/avatars/${userDataResponse.data.id}/${userDataResponse.data.avatar}.png` : null
-        };
-
-        // Here you can save the user information to your database or session
-        // Then redirect the user to a logged-in area of your site
-
+        const user = userDataResponse.data;
+        console.log(user);
         return res.send(`
-            <div style="margin:  300px auto;
-            max-width:  400px;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            font-family: sans-serif;">
-                <h3>Welcome ${user.username}</h3>
-                <span>Email: ${user.email}</span>
-                ${user.avatar ? `<img src="${user.avatar}"/>` : ''}
+            <div style="margin: 300px auto; max-width: 400px; display: flex; flex-direction: column; align-items: center; font-family: sans-serif;">
+                ${user.avatar ? `<img src="https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}"/>` : ''}
+                <h3>Welcome ${user.global_name}</h3>
+                <!--<span>Email: ${user.email}</span>-->
             </div>
         `);
     } catch (error) {
