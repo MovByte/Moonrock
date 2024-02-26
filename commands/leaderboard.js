a68fd3ca63e9a20d7cd951f65e41b63039f12aa9
Dave9123 <dave.turmawan@outlook.com>
2024-02-26 18:50:46 +0700

    todo stuff

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,4 +1,5 @@
 const { MessageEmbed } = require('discord.js');
+const sqlite3 = require('sqlite3').verbose();
 // TODO: Communicate with server about leaderboard
 
 module.exports = {
@@ -9,10 +10,8 @@ module.exports = {
     async execute(interaction) {
         try {
             console.log("Executing leaderboard command");
-            // TODO: Get the leaderboard
-            const embed = new MessageEmbed()
-                .setTitle('Leaderboard')
-                .setDescription(`On development`);
+            // TODO: Get the leaderboard either from data.db or from remote DB
+            // TODO: Get the top 10 users with the most playtime
             await interaction.reply({ embeds: [embed] });
         } catch (error) {
             await interaction.reply({ content: 'An error occured while trying to execute this command.', ephemeral: true });
