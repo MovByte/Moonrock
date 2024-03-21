465057288d43a59cebda4454b7f645e664c12412
G9 Aerospace <g9militants@gmail.com>
2024-03-21 19:48:53 +0530

    🛠️ public/assets/css/global.css -> Added background-repeat property 🛠️ public/assets/css/index.css -> Adjusted styles for search result cards 🛠️ public/assets/js/settings.js -> Fixed indentation and syntax errors 🛠️ public/index.html -> Changed background image and added backdrop filter

--- public/assets/js/settings.js
+++ public/assets/js/settings.js
@@ -1,16 +1,16 @@
-document.getElementById("flashpointZip").value = localStorage.getItem("useFlashpointZip");
-function update(event) {
-    event.preventDefault();
-    useflashpointzip = document.getElementById("flashpointZip").value
-    if (useflashpointzip === "true") {
-        localStorage.setItem("useflashpointzip", "true");
-        alert("Saved!")
-    } else if (useflashpointzip === "false") {
-        localStorage.setItem("useflashpointzip", "false");
-        alert("Saved!")
-    } else {
-        alert("An error occurred. Please try again.")
-        console.log(useflashpointzip)
+    document.getElementById("flashpointZip").value = localStorage.getItem("useFlashpointZip");
+    function update(event) {
+        event.preventDefault();
+        useflashpointzip = document.getElementById("flashpointZip").value
+        if (useflashpointzip === "true") {
+            localStorage.setItem("useflashpointzip", "true");
+            alert("Saved!")
+        } else if (useflashpointzip === "false") {
+            localStorage.setItem("useflashpointzip", "false");
+            alert("Saved!")
+        } else {
+            alert("An error occurred. Please try again.")
+            console.log(useflashpointzip)
+        }
     }
-}
-document.getElementById("settings").addEventListener("change", update);
+    document.getElementById("settings").addEventListener("change", update);
